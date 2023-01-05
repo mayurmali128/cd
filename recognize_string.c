5d585729cbc58f4f0bcbbfe6fb99032b2292975c
Mayur Mali <69722241+mayurmali128@users.noreply.github.com>
2023-01-05 22:01:26 +0530

    Add files via upload

---
+++ recognize_string.c
@@ -0,0 +1,89 @@
+/*****************************************************************************
+Name: Mayur Prakash Mali
+PRN: 1941032
+Batch: B3
+Class: L.Y. B.Tech.
+Sub: CDL
+Aim: Write a C program to recognize strings under 'a*','a*b+', 'abb'.
+*****************************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+void main()
+{
+    char s[20], c;
+    int state = 0, i = 0;
+    printf("\n Enter a string:");
+    gets(s);
+    while (s[i] != '\0')
+    {
+        switch (state)
+        {
+        case 0:
+            c = s[i++];
+            if (c == 'a')
+                state = 1;
+            else if (c == 'b')
+                state = 2;
+            else
+                state = 6;
+            break;
+        case 1:
+            c = s[i++];
+            if (c == 'a')
+                state = 3;
+            else if (c == 'b')
+                state = 4;
+            else
+                state = 6;
+            break;
+        case 2:
+            c = s[i++];
+            if (c == 'a')
+                state = 6;
+            else if (c == 'b')
+                state = 2;
+            else
+                state = 6;
+            break;
+        case 3:
+            c = s[i++];
+            if (c == 'a')
+                state = 3;
+            else if (c == 'b')
+                state = 2;
+            else
+                state = 6;
+            break;
+        case 4:
+            c = s[i++];
+            if (c == 'a')
+                state = 6;
+            else if (c == 'b')
+                state = 5;
+            else
+                state = 6;
+            break;
+        case 5:
+            c = s[i++];
+            if (c == 'a')
+                state = 6;
+            else if (c == 'b')
+                state = 2;
+            else
+                state = 6;
+            break;
+        case 6:
+            printf("\n %s is not recognised.", s);
+            exit(0);
+        }
+    }
+    if ((state == 1) || (state == 3))
+        printf("\n %s is accepted under rule 'a*'", s);
+    else if ((state == 2) || (state == 4))
+        printf("\n %s is accepted under rule 'a*b+'", s);
+    else if (state == 5)
+        printf("\n %s is accepted under rule 'abb'", s);
+    else
+        printf("\n String not accepted by automata.");
+}
